f36694d1d74e3d1801c68f7c8b0826d64275c7b2
Guillaume <guillaume.meurillon@student.howest.be>
2020-07-16 14:32:17 +0200

    Add function to create contribution

--- database/database.go
+++ database/database.go
@@ -22,7 +22,8 @@ type Database struct {
 
 // User : Struct to respresent a user object from the database
 type User struct {
-	ID                int
+	ID                string
+	UserIdentifier    string
 	Provider          string
 	ProviderUser      string
 	AccessToken       string
@@ -30,6 +31,25 @@ type User struct {
 	TokenCreationDate string
 	ExpiresAt         int
 	ExpiresIn         int
+}
+
+// Contribution : Struct to respresent a contribution object from the database
+type Contribution struct {
+	ContributionID string
+	UserAgent      string
+	Distance       int
+	TimeStampStart string
+	TimeStampStop  string
+	Duration       int
+	PointsGeom     []byte
+	PointsTime     []byte
+}
+
+// UserContribution : Struct to respresent a UserContribution object from the database
+type UserContribution struct {
+	UserContributionID string
+	UserID             string
+	ContributionID     string
 }
 
 // getDBConnectionString : Generate
@@ -64,6 +84,70 @@ func (db Database) GetUserData(userID string) (usr User, err error) {
 	if err != nil {
 		return
 	}
-	err = connection.QueryRow("SELECT \"AccessToken\" FROM public.\"Users\" where \"ProviderUser\"=$1", userID).Scan(&usr.AccessToken)
+	err = connection.QueryRow("SELECT \"UserId\", \"AccessToken\" FROM \"Users\" where \"ProviderUser\"=$1", userID).Scan(&usr.ID, &usr.AccessToken)
+	return
+}
+
+// GetNewContributionID : Get the count of current contributions
+func (db Database) GetNewContributionID() (id string, err error) {
+	connection, err := sql.Open("postgres", db.getDBConnectionString())
+	if err != nil {
+		return
+	}
+	err = connection.QueryRow("SELECT Count(1) FROM \"Contributions\";").Scan(&id)
+	return
+}
+
+// GetNewUserContributionID : Get the count of current contributions
+func (db Database) GetNewUserContributionID() (id string, err error) {
+	connection, err := sql.Open("postgres", db.getDBConnectionString())
+	if err != nil {
+		return
+	}
+	err = connection.QueryRow("SELECT Count(1) FROM \"UserContributions\";").Scan(&id)
+	return
+}
+
+// AddContribution : Create new user contribution
+func (db Database) AddContribution(contribution Contribution, user User) (err error) {
+	// Generate IDs
+	newUserContribID, err := db.GetNewUserContributionID()
+	if err != nil {
+		return
+	}
+
+	// Create contributions
+	userContrib := UserContribution{
+		UserID:             user.ID,
+		ContributionID:     contribution.ContributionID,
+		UserContributionID: newUserContribID,
+	}
+
+	// Connect to database
+	connection, err := sql.Open("postgres", db.getDBConnectionString())
+	if err != nil {
+		return
+	}
+
+	// Write Contribution
+	query := `
+	INSERT INTO "Contributions"
+	("ContributionId", "UserAgent", "Distance", "TimeStampStart", "TimeStampStop", "Duration", "PointsGeom", "PointsTime")
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+	`
+	if _, err = connection.Query(query, &contribution.ContributionID, &contribution.UserAgent, &contribution.Distance, &contribution.TimeStampStart, &contribution.TimeStampStop, &contribution.Duration, &contribution.PointsGeom, &contribution.PointsTime); err != nil {
+		log.Warnf("Could not insert value into contributions: %v", err)
+	}
+
+	// Write WriteUserContribution
+	query = `
+	INSERT INTO "UserContributions"
+	("UserContributionId", "UserId", "ContributionId")
+	VALUES ($1, $2, $3)
+	`
+	if _, err = connection.Query(query, &userContrib.UserContributionID, &userContrib.UserID, &userContrib.ContributionID); err != nil {
+		log.Warnf("Could not insert value into contributions: %v", err)
+	}
+
 	return
 }
