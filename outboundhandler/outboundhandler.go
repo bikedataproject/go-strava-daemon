40582560e64b5c5161fac0da651678bea34c5fe2
Guillaume <guillaume.meurillon@student.howest.be>
2020-07-30 09:51:44 +0200

    Add comment & refactor

--- outboundhandler/outboundhandler.go
+++ outboundhandler/outboundhandler.go
@@ -151,12 +151,13 @@ func (conf StravaHandler) RefreshUserSubscription(user *dbmodel.User) (newUser d
 	}
 
 	newUser = dbmodel.User{
-		ID:               user.ID,
-		UserIdentifier:   user.UserIdentifier,
-		AccessToken:      msg.AccessToken,
-		RefreshToken:     msg.RefreshToken,
-		ExpiresAt:        msg.ExpiresAt,
-		ExpiresIn:        msg.ExpiresIn,
+		ID:             user.ID,
+		UserIdentifier: user.UserIdentifier,
+		AccessToken:    msg.AccessToken,
+		RefreshToken:   msg.RefreshToken,
+		ExpiresAt:      msg.ExpiresAt,
+		ExpiresIn:      msg.ExpiresIn,
+		// User does not expire before 5 hours in the database, data is fetched within 10seconds
 		IsHistoryFetched: true,
 	}
 
